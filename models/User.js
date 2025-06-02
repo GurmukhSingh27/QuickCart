cfaf502ed3ba1d2b35ce8751fb30687132dae14a
Gurmukh Singh Dadiala <busygurmukh@gmail.com>
2025-06-02 14:16:14 +0530

    user.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
-import mongoose, { Schema } from "mongoose";
-import { unique } from "next/dist/build/utils";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     name: { type: String, required: true },
-    email: { type: String, required: true, unique },
+    email: { type: String, required: true, unique: true },
     imageUrl: { type: String, required: true },
     cartItems: { type: Object, default: {} },
 }, { minimize: false });
