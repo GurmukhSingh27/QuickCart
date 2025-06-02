ac551ab1fd4f33632294955ae46903e4a0b0af55
Gurmukh Singh Dadiala <busygurmukh@gmail.com>
2025-06-02 13:58:12 +0530

    done

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,10 @@
-"use client";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
-import { Toaster } from "react-hot-toast";
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from "@clerk/nextjs";
+import ToasterClient from "@/components/ToasterClient"; // 👈 import client wrapper
 
-const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
 
 export const metadata = {
   title: "QuickCart - GreatStack",
@@ -16,11 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${outfit.className} antialiased text-gray-700`} >
-          <Toaster />
-          <AppContextProvider>
-            {children}
-          </AppContextProvider>
+        <body className={`${outfit.className} antialiased text-gray-700`}>
+          <ToasterClient /> {/* 👈 only use Toaster in client component */}
+          <AppContextProvider>{children}</AppContextProvider>
         </body>
       </html>
     </ClerkProvider>
